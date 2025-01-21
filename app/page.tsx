78180f9e1b88e89fbcdefa8cd55c2664f47d1b2a
Qiniso Xulu <133514627+qinisoxulu@users.noreply.github.com>
2025-01-21 08:53:34 +0200

    Delete app directory

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import ClientHome from './components/ClientHome'
-import { getLatestProjects } from '../lib/github'
-
-
-export default async function Home() {
-  const projects = await getLatestProjects('qinisonhlakaniphoxulu')
-
-  return <ClientHome projects={projects} />
-}
